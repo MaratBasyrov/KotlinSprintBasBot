86c4f7e82ef1777b7a383f10a2a4116dc4172692
marat <basyrovm@yandex.ru>
2023-09-16 00:06:43 +0400

    lesson1-task5

---
+++ src/main/kotlin/lesson1/lesson1-task5.kt
@@ -0,0 +1,10 @@
+package lesson1
+
+fun main(){
+    val seconds:Int = 6480
+    val secondsSurplus =6480%60
+    val minute: Int = seconds/60
+    val minuteSurplus = minute%60
+    val hour = minute/60
+    println("0"+hour+":"+minuteSurplus+":"+"0"+secondsSurplus)
+}
