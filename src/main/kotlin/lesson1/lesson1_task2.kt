d5cf2be9f1a9f10444db5b6051d5ee40e9f5731e
marat <basyrovm@yandex.ru>
2023-09-15 19:17:55 +0400

    lesson1---task2

--- src/main/kotlin/lesson1/lesson1_task2.kt
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -3,7 +3,7 @@ package lesson1
 fun main () {
     val onlineOrderings = 75
     val textThankss ="Спасибо за покупку !"
-    var workMan = 2000
+    var workMan = 2001
     println(onlineOrderings)
     println(textThankss)
     //println(workMan)
