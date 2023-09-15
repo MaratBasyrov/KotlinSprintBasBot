73eaf66225091ff4f9f2d7cbcf226850ffa150c9
marat <basyrovm@yandex.ru>
2023-09-15 19:37:05 +0400

    lesson1-task2

---
+++ src/main/kotlin/lesson1/lesson1 task2.kt
@@ -0,0 +1,11 @@
+package lesson1
+
+fun main () {
+    val onlineOrderings = 75
+    val textThankss = "Спасибо за покупку !"
+    var workMan = 2000
+    println(onlineOrderings)
+    println(textThankss)
+    //println(workMan)
+    workMan = 1999
+}
