a77cff91f732e6cc588ac162bb6135c33883443b
marat <basyrovm@yandex.ru>
2023-09-16 18:39:07 +0400

    lesson3_task3

---
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -0,0 +1,19 @@
+package lesson3
+
+fun main(){
+    var number: Int
+    number = 9
+    var multiply = """
+        $number * 1 = ${number * 1}
+        $number * 2 = ${number * 2}
+        $number * 3 = ${number * 3}
+        $number * 4 = ${number * 4}
+        $number * 5 = ${number * 5}
+        $number * 6 = ${number * 6}
+        $number * 7 = ${number * 7}
+        $number * 8 = ${number * 8}
+        $number * 9 = ${number * 9}
+    """.trimIndent()
+
+    println(multiply)
+}
