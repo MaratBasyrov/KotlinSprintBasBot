73131ce1d91966733d16eaa6b56240d7b3d03828
marat <basyrovm@yandex.ru>
2023-09-16 18:23:06 +0400

    lesson3_task2

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,14 @@
+package lesson3
+
+fun main(){
+    var girlName = "Татьяна"
+    var girlSurnameBefor = "Андреева"
+    var girlSurnameNow = "Сидорова"
+    var patronymic = "Сергеевна"
+
+    var girlOldBefore = 20
+    var girlOldNow = 22
+
+    println("В $girlOldBefore лет девушку звали $girlOldBefore $girlName $patronymic")
+    println("В $girlOldNow года девушку зовут $girlOldNow $girlName $patronymic")
+}
