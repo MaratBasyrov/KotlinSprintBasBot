c0fa2a558a0ba0d5885d3b739c113ae1ff0516b0
marat <basyrovm@yandex.ru>
2023-09-16 15:02:51 +0400

    lesson2_task1

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,14 @@
+package lesson2
+
+fun main(){
+
+    var olegScore: Float = 3f
+    var mishaScore: Float = 4f
+    var dimaScore: Float = 3f
+    var viktorScore: Float = 5f
+
+    var averageScore: Float = (olegScore + mishaScore + dimaScore + viktorScore)/4
+
+    println("Средний балл $averageScore")
+
+}
