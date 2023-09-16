44716c041f10083e197d46ba03cf567ef0472c27
marat <basyrovm@yandex.ru>
2023-09-16 15:19:53 +0400

    lesson2_task2

---
+++ src/main/kotlin/lesson2/lesson2_task2.kt
@@ -0,0 +1,18 @@
+package lesson2
+
+fun main(){
+
+    var workers = 50
+    var interns = 30
+    var walkersLaborCost = 30000
+    var internsLaborCost = 20000
+
+    var workersSumCost = workers * walkersLaborCost
+    var sumCost = workersSumCost + (interns * internsLaborCost)
+    var averageCost = sumCost / (workers + interns)
+
+    println("Расходы на постоянных сотрудников $workersSumCost руб.")
+    println("Общие расходы $sumCost руб.")
+    println("Средня запрлата $averageCost руб.")
+
+}
