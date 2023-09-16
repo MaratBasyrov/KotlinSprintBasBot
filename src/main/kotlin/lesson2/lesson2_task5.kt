e6b1e9c9d6b7bb480283c490b0732ffe85d666c7
marat <basyrovm@yandex.ru>
2023-09-16 17:26:43 +0400

    lesson2_task5

---
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -0,0 +1,15 @@
+package lesson2
+
+import kotlin.math.pow
+
+fun main(){
+    var clientSum: Double = 70000.0
+    var year: Double = 20.0
+    var percent: Double = 16.7
+    percent = (1 + percent / 100)
+    var a: Double  = percent.pow(year)
+    var gain: Double = clientSum * a
+    //var b = gain.toFloat()
+
+    println(" Ваш доход за $year лет равен ${"%.3f".format(gain)}")
+}
