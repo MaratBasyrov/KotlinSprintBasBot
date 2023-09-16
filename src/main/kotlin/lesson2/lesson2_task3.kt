054c2bba6382531f99721a489109bc270731bfb3
marat <basyrovm@yandex.ru>
2023-09-16 16:03:55 +0400

    lesson2_task3

---
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -0,0 +1,20 @@
+package lesson2
+
+fun main(){
+    var exitHour = 9
+    var exitMinute = 39
+    var travelMinute = 457
+
+    var travelTimeHour = travelMinute / 60
+    var travelTimeMinute = travelMinute % 60
+
+
+    var arriveHour = exitHour + travelTimeHour
+    var arriveMinute = exitMinute + travelTimeMinute
+
+    arriveHour += arriveMinute / 60
+    arriveMinute = arriveMinute % 60
+
+    println("Прибытие поезда в $arriveHour : $arriveMinute")
+
+}
