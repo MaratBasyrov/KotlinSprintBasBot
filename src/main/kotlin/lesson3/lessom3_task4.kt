25d6ae713093db7545aabf5af947b893d4a8c490
marat <basyrovm@yandex.ru>
2023-09-16 18:56:31 +0400

    lesson3_task4

---
+++ src/main/kotlin/lesson3/lessom3_task4.kt
@@ -0,0 +1,15 @@
+package lesson3
+
+fun main(){
+    var positionStart: String
+    var positionEnd: String
+    var numberMotion: Int
+
+    positionStart = "E2"
+    positionEnd = "E4"
+    numberMotion = 1
+
+    println("$positionStart , $positionEnd, ход номер $numberMotion")
+    println("Если фигура на D2 , то при перемещении на 1 клетку откажется на D3")
+
+}
