951f198e0e41010d6c59dc28d2c10b8adc7e952b
marat <basyrovm@yandex.ru>
2023-09-16 23:17:41 +0400

    lesson2_task2 (corrected)

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -1,13 +1,13 @@
 package lesson2
 
-fun main(){
+fun main() {
 
     var olegScore: Float = 3f
     var mishaScore: Float = 4f
     var dimaScore: Float = 3f
     var viktorScore: Float = 5f
 
-    var averageScore: Float = (olegScore + mishaScore + dimaScore + viktorScore)/4
+    var averageScore: Float = (olegScore + mishaScore + dimaScore + viktorScore) / 4
 
     println("Средний балл $averageScore")
 
