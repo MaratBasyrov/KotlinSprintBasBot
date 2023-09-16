7e237c454f1005981ce28c3baf040e94fdfa9431
marat <basyrovm@yandex.ru>
2023-09-17 00:37:12 +0400

    lesson2_task5 (corrected)

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -2,12 +2,12 @@ package lesson2
 
 import kotlin.math.pow
 
-fun main(){
+fun main() {
     var clientSum: Double = 70000.0
     var year: Double = 20.0
     var percent: Double = 16.7
     percent = (1 + percent / 100)
-    var a: Double  = percent.pow(year)
+    var a: Double = percent.pow(year)
     var gain: Double = clientSum * a
     //var b = gain.toFloat()
 
