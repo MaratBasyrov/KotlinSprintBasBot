12ba69ee351b790ded3f80ea7e254111a0de0802
marat <basyrovm@yandex.ru>
2023-09-16 23:20:29 +0400

    lesson2_task3 (corrected)

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,13 +1,12 @@
 package lesson2
 
-fun main(){
+fun main() {
     var exitHour = 9
     var exitMinute = 39
     var travelMinute = 457
 
     var travelTimeHour = travelMinute / 60
     var travelTimeMinute = travelMinute % 60
-
 
     var arriveHour = exitHour + travelTimeHour
     var arriveMinute = exitMinute + travelTimeMinute
