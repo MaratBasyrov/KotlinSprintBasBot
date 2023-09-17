4f4506086537652569fdccb16394fcd46f291918
marat <basyrovm@yandex.ru>
2023-09-17 21:42:27 +0400

    lesson4_task1 (corrected)

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -5,8 +5,8 @@ fun main() {
     val tudayBooking = 13
     val tomorrowBooking = 9
 
-    val reservationTuday: Boolean = tudayBooking < NUMBER_OF_TABLES
-    val reservationTomorrow: Boolean = tomorrowBooking < NUMBER_OF_TABLES
+    val reservationTuday = tudayBooking < NUMBER_OF_TABLES
+    val reservationTomorrow = tomorrowBooking < NUMBER_OF_TABLES
 
     println("Доступность столиков на сегодня $reservationTuday")
     println("Доступность столиков на завтра $reservationTomorrow")
