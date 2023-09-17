4c6d2c372b503c967588586adcb8963e68f5aa3e
marat <basyrovm@yandex.ru>
2023-09-17 21:41:07 +0400

    lesson4_task1 (corrected)

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -1,12 +1,12 @@
 package lesson4
 
-fun main(){
+fun main() {
 
-    var tudayBooking = 13
-    var tomorrowBooking = 9
+    val tudayBooking = 13
+    val tomorrowBooking = 9
 
-    var reservationTuday: Boolean = tudayBooking < NUMBER_OF_TABLES
-    var reservationTomorrow: Boolean = tomorrowBooking < NUMBER_OF_TABLES
+    val reservationTuday: Boolean = tudayBooking < NUMBER_OF_TABLES
+    val reservationTomorrow: Boolean = tomorrowBooking < NUMBER_OF_TABLES
 
     println("Доступность столиков на сегодня $reservationTuday")
     println("Доступность столиков на завтра $reservationTomorrow")
