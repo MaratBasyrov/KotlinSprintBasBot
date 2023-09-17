6ee936a37336b2d564a7a82d05bbf30786aefcc0
marat <basyrovm@yandex.ru>
2023-09-17 21:35:55 +0400

    lesson3_task4 (corrected)

--- src/main/kotlin/lesson3/lessom3_task4.kt
+++ src/main/kotlin/lesson3/lessom3_task4.kt
@@ -1,15 +1,10 @@
 package lesson3
 
 fun main() {
-    var positionStart: String
-    var positionEnd: String
-    var numberMotion: Int
-
-    positionStart = "E2"
-    positionEnd = "E4"
-    numberMotion = 1
+    val positionStart = "E2"
+    val positionEnd = "E4"
+    val numberMotion = 1
 
     println("$positionStart , $positionEnd, ход номер $numberMotion")
     println("Если фигура на D2 , то при перемещении на 1 клетку окажется на D3")
-
 }
