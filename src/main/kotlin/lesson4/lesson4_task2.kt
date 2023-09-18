f9f1df1ae46654478f85f6c37203cf88a6e7fb70
marat <basyrovm@yandex.ru>
2023-09-18 17:49:14 +0400

    lesson4_task2 (final corrected)

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,9 +1,6 @@
 package lesson4
 
 fun main() {
-    val gravityMin = 35
-    val gravityMax = 100
-    val volumeGravityMax = 100
 
     val cargoGravityOne = 20
     val cargoVilumeOne = 80
@@ -11,7 +8,15 @@ fun main() {
     val cargoGravityTwo = 50
     val cargoVilumeTwo = 100
 
-    println("Груз с весом $cargoGravityOne кг и объемом $cargoVilumeOne л соответствует категории 'Average': ${(cargoGravityOne > gravityMin) && (cargoGravityOne < gravityMax) && (cargoVilumeOne <= volumeGravityMax)}")
-    println("Груз с весом $cargoGravityTwo кг и объемом $cargoVilumeTwo л соответствует категории 'Average': ${(cargoGravityTwo > gravityMin) && (cargoGravityTwo < gravityMax) && (cargoVilumeTwo <= volumeGravityMax)}")
+    val checkingOne =
+        (cargoGravityOne > GRAVITY_MIN) && (cargoGravityOne < GRAVITY_MAX) && (cargoVilumeOne <= VOLUE_GRAVITY_MAX)
+    val checkingTwo =
+        (cargoGravityTwo > GRAVITY_MIN) && (cargoGravityTwo < GRAVITY_MAX) && (cargoVilumeTwo <= VOLUE_GRAVITY_MAX)
+
+    println("Груз с весом $cargoGravityOne кг и объемом $cargoVilumeOne л соответствует категории 'Average': $checkingOne")
+    println("Груз с весом $cargoGravityTwo кг и объемом $cargoVilumeTwo л соответствует категории 'Average': $checkingTwo")
 }
 
+const val GRAVITY_MIN = 35
+const val GRAVITY_MAX = 100
+const val VOLUE_GRAVITY_MAX = 100
