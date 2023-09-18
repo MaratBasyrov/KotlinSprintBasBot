465c82d1398ca0c07c116f829018dad17e640b8f
marat <basyrovm@yandex.ru>
2023-09-18 18:07:36 +0400

    lesson4_task3 (final corrected)

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -2,12 +2,18 @@ package lesson4
 
 fun main() {
 
-    val sunnyDay: Boolean = true
-    val openTent: Boolean = true
-    val humidity: Int = 20
-    val season: String = "зима"
+    val isSunnyForBob = true
+    val isOpenTentForBob = true
+    val humidityForBob = 20
+    val seasonForBob = "зима"
 
-    val analysisBob = (sunnyDay == true) && (openTent == true) && (humidity == 20) && (season !== "зима")
+    val analysisBob =
+        (IS_SUNNY_DAY == isSunnyForBob) && (IS_OPEN_TENT == isOpenTentForBob) && (HUMIDITY_PERCENT == humidityForBob) && (SEASON_YAER !== "зима")
 
     print("Благоприятные ли условия сейчас для роста бобовых? $analysisBob")
 }
+
+const val IS_SUNNY_DAY: Boolean = true
+const val IS_OPEN_TENT: Boolean = true
+const val HUMIDITY_PERCENT: Int = 20
+const val SEASON_YAER: String = "зима"
