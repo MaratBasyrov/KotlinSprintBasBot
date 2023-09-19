f10f46ead62070e3460e97b01a1a77f85ef78903
marat <basyrovm@yandex.ru>
2023-09-19 22:39:26 +0400

    lesson4_task5

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -3,24 +3,25 @@ package lesson4
 fun main() {
 
     print("Имеюются ли повреждения: ")
-    val enterDamage = readln().toBoolean()
+    val isEnterDamage = readln().toBoolean()
     print("Количество экипажа: ")
     val enterCrew = readln().toInt()
     print("Количество ящиков с провизией: ")
     val enterFood = readln().toInt()
     print("Хорошие погодные условия: ")
-    val enterWeather: Boolean = readln().toBoolean()
+    val isEnterWeather: Boolean = readln().toBoolean()
 
-    if ((enterDamage == IS_DAMAGE) && (enterCrew in CREW_MIN..CREW_MAX) && (enterFood >= FOOD) && (enterWeather == IS_WEATHER))
+    if ((isEnterDamage == IS_DAMAGE) && (enterCrew in CREW_MIN..CREW_MAX) && (enterFood >= FOOD) && (isEnterWeather == IS_WEATHER))
         println("Корабль может отправиться в путешествие")
-    if ((enterDamage !== IS_DAMAGE) && (enterCrew in CREW_MIN..CREW_MAX) && (enterFood >= FOOD) && (enterWeather == IS_WEATHER))
+    else
+    if ((isEnterDamage !== IS_DAMAGE) && (enterCrew in CREW_MIN..CREW_MAX) && (enterFood >= FOOD) && (isEnterWeather == IS_WEATHER))
         println("Корабль может отправиться в путешествие Альтернативно")
     else println("К сожалению, поездка невозможна")
 
 }
 
-val IS_DAMAGE: Boolean = false
-val CREW_MIN: Int = 55
-val CREW_MAX: Int = 70
-val FOOD: Int = 50
-val IS_WEATHER: Boolean = true
+const val IS_DAMAGE: Boolean = false
+const val CREW_MIN: Int = 55
+const val CREW_MAX: Int = 70
+const val FOOD: Int = 50
+const val IS_WEATHER: Boolean = true
