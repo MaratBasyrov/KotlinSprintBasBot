f0194005012fa065b7ae8b717c0f49cd477ad49c
marat <basyrovm@yandex.ru>
2023-09-22 00:06:18 +0400

    lesson6_task1

---
+++ src/main/kotlin/lesson6/lesson6_task1.kt
@@ -0,0 +1,22 @@
+package lesson6
+
+fun main() {
+    print("Создайте логин : ")
+    val clientLogin: String = readln()
+    print("Создайте пароль : ")
+    val clientPassword: String = readln()
+
+    print("Введите логин : ")
+    var enterLogin: String = readln()
+    print("Введите пароль : ")
+    var enterPassword: String = readln()
+
+    while ((enterLogin != clientLogin) && (enterPassword != clientPassword)) {
+        println("Данные не совпадают, попробуйте еще раз ")
+        print("Введите логин : ")
+        enterLogin = readln()
+        print("Введите пароль : ")
+        enterPassword = readln()
+    }
+    println("Авторизация прошла успешно")
+}
