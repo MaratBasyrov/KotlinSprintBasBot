93bab23320f0c21497bf55fa91519145fff7944b
marat <basyrovm@yandex.ru>
2023-09-22 09:48:15 +0400

    lesson6_task4

---
+++ src/main/kotlin/lesson6/lesson6_task4.kt
@@ -0,0 +1,28 @@
+package lesson6
+
+fun main() {
+    val numberGeneration = (1..9).random()
+    var numberAttempts = 5
+
+    print("Угадайте число от 1 до 9 : ")
+    var enterNumber = readln().toInt()
+
+    if (enterNumber == numberGeneration) {
+        println("победа")
+
+         } else {
+         --numberAttempts
+                 while (numberAttempts > 0) {
+                         print("$numberGeneration попробуйте еще раз: ")
+                         enterNumber = readln().toInt()
+                             if (enterNumber == numberGeneration) {
+                                 println("это победа")
+                                 break
+                             }
+                 numberAttempts--
+                 }
+         println("проиграли, было загадано $numberGeneration")
+         }
+    println("Игра окончена")
+
+}
