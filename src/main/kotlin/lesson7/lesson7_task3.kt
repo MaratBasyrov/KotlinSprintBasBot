4f8d3f8baf10a32cfae412baa9a5223936b8f60e
marat <basyrovm@yandex.ru>
2023-09-25 11:26:52 +0400

    lesson7_task3

--- src/main/kotlin/lesson7/lesson7_task3.kt
+++ src/main/kotlin/lesson7/lesson7_task3.kt
@@ -3,9 +3,7 @@ package lesson7
 fun main() {
     print("Введите лимит : ")
     val limitNumber = readln().toInt()
-    for (i in 0..limitNumber)
-        while (i % 2 == 0) {
-            println(i)
-            break
-        }
+    for (i in 0..limitNumber step 2) {
+        println(i)
+    }
 }
