00220914c1366c629ab505f88d8381aaa157a389
marat <basyrovm@yandex.ru>
2023-09-26 21:22:29 +0400

    lesson9_task1 (corrected)

--- src/main/kotlin/lesson9/lesson9_task1.kt
+++ src/main/kotlin/lesson9/lesson9_task1.kt
@@ -4,6 +4,6 @@ fun main() {
     val newIngredients = listOf("перец", "соль", "мука", "вода")
     println("\nВ рецепте есть следующие ингредиенты: $newIngredients")
     newIngredients.forEach {
-        println("$it")
+        println(it)
     }
 }
