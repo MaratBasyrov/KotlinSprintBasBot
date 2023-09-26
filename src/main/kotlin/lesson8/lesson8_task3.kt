36b725f23afe34e84cf34e64f908b7758a607cdd
marat <basyrovm@yandex.ru>
2023-09-26 18:20:36 +0400

    lesson8_task3

--- src/main/kotlin/lesson8/lesson8_task3.kt
+++ src/main/kotlin/lesson8/lesson8_task3.kt
@@ -2,11 +2,10 @@ package lesson8
 
 fun main() {
     val ingredients = arrayOf("чеснок", "соль", "лук", "вода", "перец")
-    print("Ввдите ингредиент который хотите проверить: ")
-    val enterIngredients = readln().toString()
+    print("Введите ингредиент который хотите проверить: ")
+    val enterIngredients = readln()
 
-    val isIngredients = enterIngredients in ingredients
-    if (isIngredients == true) {
+    if (enterIngredients in ingredients) {
         println("Ингредиент $enterIngredients в рецепте есть")
-    } else println("Такого ингредиента в рецепте не")
+    } else println("Такого ингредиента в рецепте нет")
 }
