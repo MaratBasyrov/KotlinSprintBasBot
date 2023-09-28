78498dce0da6677e7ec56cce4e9194090a7bce4c
marat <basyrovm@yandex.ru>
2023-09-28 08:29:14 +0400

    lesson9_task5

--- src/main/kotlin/lesson9/lesson9_task5.kt
+++ src/main/kotlin/lesson9/lesson9_task5.kt
@@ -2,9 +2,8 @@ package lesson9
 
 fun main() {
     println("Введи 5 ингредиентов:")
-    val listNew = List(5) { readln() }.toMutableList()
-    val deleteDuplicate = listNew.distinct()
-    val sortedList = deleteDuplicate.sorted()
+    val listNew = List(5) { readln() }.toMutableSet()
+    val sortedList = listNew.sorted()
     val listToStr = sortedList.joinToString(", ")
     val strUpperCase = listToStr.replaceFirstChar { it.uppercase() }
     val strDod = strUpperCase.plus(".")
