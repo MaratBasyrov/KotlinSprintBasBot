4bf9a1248e5fffa5f785f606ce7bd323182054e7
marat <basyrovm@yandex.ru>
2023-09-28 08:16:29 +0400

    lesson9_task4

--- src/main/kotlin/lesson9/lesson9_task4.kt
+++ src/main/kotlin/lesson9/lesson9_task4.kt
@@ -2,8 +2,6 @@ package lesson9
 
 fun main() {
     println("Запишите 5 ингредиентов через запятую: ")
-    val ingredients = readln()
-    val listGeneration = ingredients.split(",")
-    val listSort = listGeneration.sorted()
-    println(listSort)
+    val ingredients = readln().split(",").sorted()
+    println(ingredients)
 }
