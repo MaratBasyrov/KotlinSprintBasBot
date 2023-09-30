2a6fd1ec315db9198d08d3c64b0b540432e01528
marat <basyrovm@yandex.ru>
2023-09-30 22:47:48 +0400

    lesson11_task2

--- src/main/kotlin/lesson11/UserData.kt
+++ src/main/kotlin/lesson11/UserData.kt
@@ -3,6 +3,32 @@ package lesson11
 class UserData(
     val id: Int,
     val userLogin: String,
-    val userPassword: String,
+    var userPassword: String,
     val userEmail: String,
-)
+    var bio: String = "",
+) {
+    fun outputUser() {
+        println(
+            "Данные %d пользователя: логин \'%s\', пароль \'%s\', e-mail \'%s\'".format(
+                id, userLogin, userPassword, userEmail
+            )
+        )
+    }
+
+    fun enterBio() {
+        println("введите данные в свойсво Bio: ")
+        bio = readln()
+    }
+
+    fun enterNewPassword() {
+        println("Для изменения пароля введите действующий: ")
+        if (userPassword == readln()) {
+            println("Придумайте новый: ")
+            userPassword = readln()
+        } else println("ввел неправильный пароль")
+    }
+
+    fun enterUserMassage(message: String) {
+        println(message)
+    }
+}
--- src/main/kotlin/lesson11/main.kt
+++ src/main/kotlin/lesson11/main.kt
@@ -8,21 +8,8 @@ fun main() {
         userEmail = "vovka@mail.ru",
     )
 
-    val userTwo: UserData = UserData(
-        id = 2,
-        userLogin = "Kolya",
-        userPassword = "yh654",
-        userEmail = "kolyan@mail.ru",
-    )
-    println(
-        "Данные %d пользователя: логин \'%s\', пароль \'%s\', e-mail \'%s\'".format(
-            userOne.id, userOne.userLogin, userOne.userPassword, userOne.userEmail
-        )
-    )
-
-    println(
-        "Данные %d пользователя: логин \'%s\', пароль \'%s\', e-mail \'%s\'".format(
-            userTwo.id, userTwo.userLogin, userTwo.userPassword, userTwo.userEmail
-        )
-    )
+    userOne.outputUser()
+    userOne.enterBio()
+    userOne.enterNewPassword()
+    userOne.enterUserMassage("привет")
 }
