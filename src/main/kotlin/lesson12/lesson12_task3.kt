c3027a95af060220f4d88ab8fa4912abd4bd31b9
marat <basyrovm@yandex.ru>
2023-10-06 01:43:46 +0400

    lesson12_task3

---
+++ src/main/kotlin/lesson12/lesson12_task3.kt
@@ -0,0 +1,29 @@
+package lesson12
+
+
+fun main() {
+    var today: Weather = Weather("+45", "+30", "754 м.р.с")
+    var tomorrow: Weather = Weather("+25", "+10", "651 м.р.с")
+    var afterTomorrow: Weather = Weather("+15", "+5", "541 м.р.с")
+
+    today.printWeather()
+    tomorrow.printWeather()
+    afterTomorrow.printWeather()
+}
+
+class Weather(val tempDay: String, val tempNigth: String, val atmPressure: String) {
+    val isRain: Boolean = false
+
+    fun printWeather() {
+        println(
+            "Температура днем %s, температура ночтью %s, дождь %s, атмосферное давление %s".format(
+                tempDay,
+                tempNigth,
+                isRain,
+                atmPressure
+            )
+        )
+    }
+
+
+}
