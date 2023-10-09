e48102455cff784bc709706875843c4bcd54f231
marat <basyrovm@yandex.ru>
2023-10-09 11:01:26 +0400

    lesson13_task3 (corrected)

--- src/main/kotlin/lesson13/lesson13_task3.kt
+++ src/main/kotlin/lesson13/lesson13_task3.kt
@@ -1,26 +1,24 @@
 package lesson13
 
 fun main() {
-    var listContackt: List<TelephoneContact>?
-    val contacktMasha: TelephoneContact = TelephoneContact("Маша", null, "Газмпром")
+
+    val contacktMasha: TelephoneContact = TelephoneContact("Маша", 89567860652, "Газмпром")
     val contacktAnna: TelephoneContact = TelephoneContact("Анна", 89567867652, "Роснефть")
     val contacktEkat: TelephoneContact = TelephoneContact("Катя", 89984554322, null)
-    listContackt = listOf(contacktMasha, contacktAnna, contacktEkat)
+    val listContackt = listOf(contacktMasha, contacktAnna, contacktEkat)
     informationContact(listContackt)
 }
 
 fun informationContact(a: List<TelephoneContact>) {
     a.forEach {
         println(
-            "Имя ${it.name ?: "[не указано]"}, Телефон ${it.numberTel ?: "[не указано]"}, Компания ${it.company ?: "[не указано]"}"
+            "Имя ${it.name}, Телефон ${it.numberTel}, Компания ${it.company ?: "[не указано]"}"
         )
     }
 }
 
 class TelephoneContact(
-    val name: String?,
-    val numberTel: Long?,
+    val name: String,
+    val numberTel: Long,
     val company: String?,
-) {
-
-}
+)
