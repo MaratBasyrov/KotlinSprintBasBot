53bbfb43585ba9b0f1848b578552a7c0710bf437
marat <basyrovm@yandex.ru>
2023-10-09 14:44:58 +0400

    lesson13_task4

---
+++ src/main/kotlin/lesson13/lesson13_task4.kt
@@ -0,0 +1,57 @@
+package lesson13
+
+fun main() {
+
+    var listContact: List<TelephoneContact> = mutableListOf()
+
+    do {
+        val enterContact = newContact()
+        if (enterContact != null) {
+             listContact.plusElement(enterContact)
+        } else println("Контакт не добавлен, т.к. нет номера")
+
+        println("Если хотите добавить новую запись, введите \"да\"")
+
+    } while (readln().uppercase() == "ДА")
+
+   outputInformationContact(listContact) // не работает
+
+
+}
+
+fun newContact(): TelephoneContact? {
+    println("введите данные контакта:")
+    print("Имя: ")
+    var nameNew = readlnOrNull()
+    if (nameNew.equals("", true)) {
+        nameNew = null
+    }
+    print("Телефон: ")
+    var numberTelNew = readlnOrNull()
+    if (numberTelNew.equals("", true)) {
+        numberTelNew = null
+    }
+    print("Компания: ")
+    var companyNew = readlnOrNull()
+    if (companyNew.equals("", true)) {
+        companyNew = null
+    } else companyNew
+
+    return if (numberTelNew != null) {
+        TelephoneContact(nameNew, numberTelNew, companyNew)
+    } else null
+}
+
+fun outputInformationContact(listClass: List<TelephoneContact>) {
+    listClass.forEach {
+        println(
+            "Имя ${it.name ?: "[не указано]"}, Телефон ${it.numberTel}, Компания ${it.company ?: "[не указано]"}"
+        )
+    }
+}
+
+class TelephoneContact(
+    val name: String?,
+    val numberTel: String?,
+    val company: String?,
+)
