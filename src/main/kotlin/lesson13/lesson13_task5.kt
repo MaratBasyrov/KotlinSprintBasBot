d6d0a96d743eb6d6d8e69471263a87006728a16b
marat <basyrovm@yandex.ru>
2023-10-09 19:13:08 +0400

    lesson13_task5

--- src/main/kotlin/lesson13/lesson13_task5.kt
+++ src/main/kotlin/lesson13/lesson13_task5.kt
@@ -1,14 +1,12 @@
 package lesson13
 
-fun main(){
+fun main() {
     try {
-
         val number: Long
         val strNumber = readLine().toString()
         number = strNumber.toLong()
-    } catch (exception: NumberFormatException){
+    } catch (exception: NumberFormatException) {
         println("Номер может содержать только цифры")
     }
-
 
 }
