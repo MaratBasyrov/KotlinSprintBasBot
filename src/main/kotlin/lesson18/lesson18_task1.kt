ff92f5279900d8b6413a636bd4a8e8cc086c2966
marat <basyrovm@yandex.ru>
2023-10-13 23:50:07 +0400

    lesson18_task1

---
+++ src/main/kotlin/lesson18/lesson18_task1.kt
@@ -0,0 +1,20 @@
+package lesson18
+
+fun main() {
+    val orderOne = Order("43553")
+    orderOne.printItem("Телевизор")
+    orderOne.printItem(listOf("Холодильник", "Утюг"))
+}
+
+class Order(
+    val numberOrder: String,
+
+    ) {
+    fun printItem(item: String) {
+        println("Заказан товар: $item")
+    }
+
+    fun printItem(item: List<String>) {
+        println("Заказаны следующие товары: ${item.joinToString { it.toString() }}")
+    }
+}
