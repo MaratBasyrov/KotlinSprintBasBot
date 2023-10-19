0363941f8347373706fe3e5a1deda887d864fc3d
marat <basyrovm@yandex.ru>
2023-10-19 15:25:43 +0400

    lesson20_task2

--- src/main/kotlin/lesson20/lesson20_task3.kt
+++ src/main/kotlin/lesson20/lesson20_task3.kt
@@ -2,9 +2,10 @@ package lesson20
 
 fun main() {
     val playerOne = Player();
-    { if (playerOne.keyDoor) println("Дверь открыта") }()
+
 }
 
 class Player() {
     val keyDoor: Boolean = true
+    init{ if (this.keyDoor) println("Дверь открыта") else println("Дверь закрыта") }
 }
